83f9eb9298a6f2f85b12f3ee2d2f534d506c2997
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2019-11-19 19:56:34 +0900

    `bundle update` and support new jekyll-last-modified-at

--- _plugins/skip_last_modified_at.rb
+++ _plugins/skip_last_modified_at.rb
@@ -8,25 +8,6 @@ module SkipLastModifiedAt
     return if %r!\Atag/! =~ page_path # jekyll-tagging
     super
   end
-
-  def last_modified_at_unix
-    if is_git_repo?(site_source)
-      last_commit_date = ::Jekyll::LastModifiedAt::Executor.sh(
-        'git',
-        '--git-dir',
-        top_level_git_directory,
-        'log',
-        '--format="%ct"',
-        '-1', ## add
-        '--',
-        relative_path_from_git_dir
-      )[/\d+/]
-      # last_commit_date can be nil iff the file was not committed.
-      (last_commit_date.nil? || last_commit_date.empty?) ? mtime(absolute_path_to_article) : last_commit_date
-    else
-      mtime(absolute_path_to_article)
-    end
-  end
 end
 class Jekyll::LastModifiedAt::Determinator
   prepend SkipLastModifiedAt
